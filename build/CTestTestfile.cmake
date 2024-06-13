d7e22f489c019842f858f38311f63060b17a15c6
Alexander <lukoshka08@gmail.com>
2024-06-13 10:17:14 +0300

    Add test and build

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/boynxtd00r/2-classes
+# Build directory: /home/boynxtd00r/2-classes/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(MyTest "/home/boynxtd00r/2-classes/build/runTests")
+set_tests_properties(MyTest PROPERTIES  _BACKTRACE_TRIPLES "/home/boynxtd00r/2-classes/CMakeLists.txt;18;add_test;/home/boynxtd00r/2-classes/CMakeLists.txt;0;")
